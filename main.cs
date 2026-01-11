a112f4b6ea4977169d068a2b2fcc972522c4a4b3
Mykhailo09Isariuk <isariuk.mykhailo@gmail.com>
2026-01-11 13:47:50 +0200

    Cycle While

---
+++ main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace project
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            int number = 1;
+            while (number <= 100)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+        }
+    }
+}
